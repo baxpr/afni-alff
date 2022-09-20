1807f3f6d8f711b2dd91789dd9cfd23765946dfb
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-09-20 14:36:38 -0500

    pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM afni/afni_make_build:AFNI_22.2.09
 
 COPY afni-alff.sh /opt/afni-alff/
+
--- afni-alff.sh
+++ afni-alff.sh
@@ -1,15 +1,49 @@
 #!/usr/bin/env bash
 
-3dRSFC -nosat -nodetrend -mask /INPUTS/rwmask.nii.gz \
-    -prefix /OUTPUTS/wremovegm 0.01 0.10 /INPUTS/wremovegm.nii.gz
+# Given a brain mask and a preprocessed fMRI image time series,
+# compute some ALFF images.
 
-gm=$(3dmaskave -mask /INPUTS/rwmask.nii.gz /OUTPUTS/wremovegm_ALFF+tlrc.HEAD)
-echo GM $gm
-3dcalc -a /OUTPUTS/wremovegm_ALFF+tlrc.HEAD -expr "a / ${gm}" \
-    -prefix /OUTPUTS/wremovegm_ALFF_norm
+# Defaults
+export mask_niigz=/INPUTS/rwmask.nii.gz
+export fmri_niigz=/INPUTS/wremovegm.nii.gz
+export bplo_hz=0.01
+export bphi_hz=0.10
+export out_dir=/OUTPUTS
 
-fgm=$(3dmaskave -mask /INPUTS/rwmask.nii.gz /OUTPUTS/wremovegm_fALFF+tlrc.HEAD)
-echo FGM $fgm
-3dcalc -a /OUTPUTS/wremovegm_fALFF+tlrc.HEAD -expr "a / ${fgm}" \
-    -prefix /OUTPUTS/wremovegm_fALFF_norm
+# Command line overrides
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in        
+        --mask_niigz)
+            export mask_niigz="$2"; shift; shift ;;
+        --fmri_niigz)
+            export fmri_niigz="$2"; shift; shift ;;
+        --bplo_hz)
+            export bplo_hz="$2"; shift; shift ;;
+        --bphi_hz)
+            export bphi_hz="$2"; shift; shift ;;
+        --out_dir)
+             export out_dir="$2"; shift; shift ;;
+         *)
+             echo "Input ${1} not recognized"
+             shift ;;
+     esac
+ done
+
+ # Compute ALFF etc
+3dRSFC -nosat -nodetrend -mask "${mask_niigz}" \
+    -prefix /OUTPUTS/rsfc 0.01 0.10 "${fmri_niigz}"
+
+# Normalizing factors
+mean_alff=$(3dmaskave -mask "${mask_niigz}" /OUTPUTS/rsfc_ALFF+tlrc.HEAD)
+mean_falff=$(3dmaskave -mask "${mask_niigz}" /OUTPUTS/rsfc_fALFF+tlrc.HEAD)
+echo Mean ALFF ${mean_alff}
+echo Mean fALFF ${mean_falff}
+
+# Normalize
+3dcalc -a /OUTPUTS/rsfc_ALFF+tlrc.HEAD -expr "a / ${mean_alff}" \
+    -prefix /OUTPUTS/rsfc_ALFF_norm
+3dcalc -a /OUTPUTS/rsfc_fALFF+tlrc.HEAD -expr "a / ${mean_falff}" \
+    -prefix /OUTPUTS/rsfc_fALFF_norm
 
