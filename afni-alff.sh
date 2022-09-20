cb935e54b3acde45275fd4773ee957dd4ab0834e
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-09-20 15:04:13 -0500

    outputs

--- afni-alff.sh
+++ afni-alff.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # Given a brain mask and a preprocessed fMRI image time series,
-# compute some ALFF images.
+# compute some ALFF images. Preprocessing would typically be by
+# https://github.com/baxpr/connprep WITHOUT bandpass filter step.
 
 # Defaults
 export mask_niigz=/INPUTS/rwmask.nii.gz
-export fmri_niigz=/INPUTS/wremovegm.nii.gz
+export fmri_niigz=/INPUTS/filtered_removegm_noscrub_wadfmri.nii.gz
 export bplo_hz=0.01
 export bphi_hz=0.10
 export out_dir=/OUTPUTS
@@ -31,19 +32,26 @@ do
      esac
  done
 
+ # Work in outputs dir
+ cd "${out_dir}"
+
  # Compute ALFF etc
 3dRSFC -nosat -nodetrend -mask "${mask_niigz}" \
-    -prefix /OUTPUTS/rsfc 0.01 0.10 "${fmri_niigz}"
+    -prefix rsfc 0.01 0.10 "${fmri_niigz}"
 
 # Normalizing factors
-mean_alff=$(3dmaskave -mask "${mask_niigz}" /OUTPUTS/rsfc_ALFF+tlrc.HEAD)
-mean_falff=$(3dmaskave -mask "${mask_niigz}" /OUTPUTS/rsfc_fALFF+tlrc.HEAD)
+mean_alff=$(3dmaskave -mask "${mask_niigz}" rsfc_ALFF+tlrc.HEAD)
+mean_falff=$(3dmaskave -mask "${mask_niigz}" rsfc_fALFF+tlrc.HEAD)
 echo Mean ALFF ${mean_alff}
 echo Mean fALFF ${mean_falff}
 
 # Normalize
-3dcalc -a /OUTPUTS/rsfc_ALFF+tlrc.HEAD -expr "a / ${mean_alff}" \
-    -prefix /OUTPUTS/rsfc_ALFF_norm
-3dcalc -a /OUTPUTS/rsfc_fALFF+tlrc.HEAD -expr "a / ${mean_falff}" \
-    -prefix /OUTPUTS/rsfc_fALFF_norm
+3dcalc -a rsfc_ALFF+tlrc.HEAD -expr "a / ${mean_alff}" -prefix rsfc_ALFF_norm
+3dcalc -a rsfc_fALFF+tlrc.HEAD -expr "a / ${mean_falff}" -prefix rsfc_fALFF_norm
+
+# Convert to nifti
+cd "${out_dir}"
+for f in *.BRIK; do
+	3dAFNItoNIFTI "${f}"
+done
 
