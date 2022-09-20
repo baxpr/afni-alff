e64fdd6b2b1da729df0186825fd835f3cee9ef89
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-09-20 15:13:13 -0500

    space

--- testrun.sh
+++ testrun.sh
@@ -2,7 +2,7 @@
 docker run \
     --mount type=bind,src=`pwd -P`/INPUTS,dst=/INPUTS \
     --mount type=bind,src=`pwd -P`/OUTPUTS,dst=/OUTPUTS \
-    baxterprogers/afni-alff:v1.0.0-beta1 \
+    baxterprogers/afni-alff:v1.0.0-beta2 \
     --mask_niigz /INPUTS/rwmask.nii.gz \
     --fmri_niigz /INPUTS/filtered_removegm_noscrub_wadfmri.nii.gz \
     --bplo_hz 0.01 \
